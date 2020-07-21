7dc85855c3d28198fe7fcc0de810da3186bc2033
w1ldy0uth <wanhelsing00@gmail.com>
2020-07-21 20:38:00 +0300

    finished arp

---
+++ arp_scan.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+# -*- coding: UTF=8 -*-
+
+from scapy.all import ARP, srp, Ether
+from datetime import datetime
+
+ip_range = "192.168.10.1/24"
+
+def arpscan():
+    print("Scan started")
+    print("-"*40 + "\n")
+
+    start_time = datetime.now()
+
+    arp = ARP(pdst=ip_range)
+    ether = Ether(dst="ff:ff:ff:ff:ff:ff")
+
+    answer = srp(ether/arp, timeout=3, verbose=0)[0]
+
+    res = []
+
+    for sent, recieved in answer:
+        res.append({'IP': recieved.psrc, 'MAC': recieved.hwsrc})
+
+    print("Devices in the network:")
+    print("IP" + 20*" " + "MAC")
+    for device in res:
+        print("{:16}     {}".format(device['IP'], device['MAC']))
+
+    print("\n" + "-"*40)
+    print("Scan ended")
+
+    end_time = datetime.now()
+
+    print("Scan duration: ", end_time - start_time)
+
+if __name__ == '__main__':
+    arpscan()

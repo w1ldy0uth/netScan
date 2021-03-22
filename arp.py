862888082bea15e64e860e744700505c79228fe3
Ivan Shurygin <shurygin1vs@gmail.com>
2021-03-23 00:24:05 +0300

    Small arp fixes

--- arp.py
+++ arp.py
@@ -2,13 +2,14 @@
 # -*- coding: UTF=8 -*-
 
 from scapy.all import ARP, srp, Ether
-from ip_getter import cidrip
+from ip_getter import cidrip, getip
 
 
 class Arp:
     def __init__(self, verbose):
         self.verbose = verbose
         self.ip = cidrip()
+        print("Your current IP address: {}\n".format(getip()))
 
     def scan(self):
         arp = ARP(pdst=self.ip)

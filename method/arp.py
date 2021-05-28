814f9e3ae0e0a532e226af7c25ae95c1ce6b4871
w1ldy0uth <shurygin1vs@gmail.com>
2021-05-28 13:13:56 +0300

    Created documentation for main methods

--- method/arp.py
+++ method/arp.py
@@ -2,29 +2,40 @@
 # -*- coding: UTF=8 -*-
 
 from scapy.all import ARP, srp, Ether
-from sub.ipget import cidr_ip, get_ip
-from sub.macget import get_mac
+from sub.ipget import cidr_ip
 
 
 class Arp:
+    """A class to recieve IP and MAC addresses of hosts in current network."""
+
     def __init__(self, verbose):
+        """
+        Constructs all the necessary attributes.
+
+        verbose: bool
+            permission for output of additional info about packets 
+        """
+
         self.verbose = verbose
         self.ip = cidr_ip()
 
     def scan(self):
+        """Scans network and pulls out IPs and MACs from recieved packets."""
+
+        # arp packets's parts (to send)
         arp = ARP(pdst=self.ip)
         ether = Ether(dst='ff:ff:ff:ff:ff:ff')
 
-        ans = srp(ether/arp, timeout=4, verbose=self.verbose)[0]
+        ans = srp(ether/arp, timeout=4, verbose=self.verbose)[0] # sending packet to network
 
-        self.res = []
+        self.res = [] # storage for addresses
 
         for snd, rcv in ans:
-            self.res.append({'IP': rcv.psrc, 'MAC': rcv.hwsrc})
+            self.res.append({'IP': rcv.psrc, 'MAC': rcv.hwsrc}) # pulling out IPs and MACs
 
         return self.res
 
 
 if __name__ == "__main__":
-    scanner = Arp(verbose=False)
+    scanner = Arp(verbose=False) 
     print(scanner.scan())
--- method/port.py
+++ method/port.py
@@ -1,19 +1,36 @@
 #!/usr/bin/env python3
 # -*- coding: UTF=8 -*-
 
-from scapy.all import IP, ICMP, TCP, sr1
-from sub.ipget import get_ip
 import threading 
 import random
 from queue import Queue
+from scapy.all import IP, ICMP, TCP, sr1
+from sub.ipget import get_ip
 
 
 class Port:
-    def __init__(self, verbose, ip, port_begin, port_end):
-        self.verbose = verbose
-        self.ip = str(ip)[:-1:]
+    """A class to recieve a state of each host's port."""
 
-        self. threads = 100
+    def __init__(self, verbose, ip, threads, port_begin, port_end):
+        """
+        Constructs all the necessary attributes.
+
+        verbose: bool
+            permission for output of additional info about packets
+        ip: string
+            IP of scannable host
+        threads: int
+            amount of threads (default - 100)
+        port_begin, port_end: int, int
+            starting and ending ports
+        open, closed, dropped: list, list, list:
+            list of each type of ports
+        """
+
+        self.verbose = verbose
+        self.ip = str(ip)
+
+        self. threads = threads
         self.print_lock = threading.Lock()
         self.q = Queue()
     
@@ -23,35 +40,41 @@ class Port:
         self.open = self.closed = self.dropped = []
 
     def scan(self, dst):
-        src = random.randint(1025, 65534)
+        """Scans each port by performing 3-way TCP hadnshake."""
+
+        src = random.randint(1025, 65534) # random port for scanning from
         rsp = sr1(
-            IP(dst=self.ip)/TCP(sport=src, dport=dst, flags='S'),
+            IP(dst=self.ip)/TCP(sport=src, dport=dst, flags='S'), # TCP packet to send
             timeout=1, verbose=0)
 
         if rsp is None:
-            self.dropped.append(self.ip + ":" + str(dst))
+            self.dropped.append(self.ip + ":" + str(dst)) # Empty response signals about firewall
 
         elif rsp.haslayer(TCP):
-            if rsp.getlayer(TCP).flags == 0x12:
+            if rsp.getlayer(TCP).flags == 0x12: # Code 12 response means that port is open
                 send_rst = sr1(
                     IP(dst=self.ip)/TCP(sport=src, dport=dst, flags='R'),
                     timeout=1, verbose=0)
                 self.open.append(self.ip + ":" + str(dst))
 
-            elif rsp.getlayer(TCP).flags == 0x14:
+            elif rsp.getlayer(TCP).flags == 0x14: # Code 14 response says that port is close
                 self.closed.append(self.ip + ":" + str(dst))
 
-        elif rsp.haslayer(ICMP):
+        elif rsp.haslayer(ICMP): # Codes 1, 2, 3, 9, 10 or 13 response signals about firewall
             if (int(rsp.getlayer(ICMP).type) == 3) and (int(rsp.getlayer(ICMP).code) in [1,2,3,9,10,13]):
                 self.dropped.append(self.ip + ":" + str(dst))
     
     def threader(self):
+        """Creates a single thread."""
+
         while True:
             current = self.q.get()
             self.scan(current)
             self.q.task_done()
     
     def work_process(self):
+        """Makes threads work and returns state of each port from range."""
+        
         for thread in range(self.threads):
             t = threading.Thread(target=self.threader)
             t.daemon = True
@@ -66,5 +89,5 @@ class Port:
 
 
 if __name__ == "__main__":
-    scan = Port(verbose=False, ip=get_ip(), port_begin=1, port_end=100)
+    scan = Port(verbose=False, ip=get_ip(), threads=100, port_begin=1, port_end=100)
     print(scan.work_process())
